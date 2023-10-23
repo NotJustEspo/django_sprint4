60ea8027dc47ca81dade3b271c2d71399d11cb7f
Espo <notjustespo@yandex.ru>
2023-10-23 09:28:34 +0300

    Fix function of Udate and Delete commits

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -7,9 +7,11 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
 from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
 from django.db.models import Count
+from django.http import Http404
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import MultipleObjectMixin
+from core.views import page_not_found
 
 from blog.models import Category, Post, User, Comment
 from .forms import PostForm, UserForm, CommentForm
@@ -205,20 +207,17 @@ class CommentUpdateView(LoginRequiredMixin, UpdateView):
     pk_url_kwarg = 'comment_id'
 
     def dispatch(self, request, *args, **kwargs):
-        self.post_obj = get_object_or_404(
-            Post,
-            id=kwargs['post_id'],)
+        instance = get_object_or_404(
+            Comment,
+            id=kwargs['comment_id'],)
+        if instance.author != request.user:
+            raise Http404
         return super().dispatch(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['post_id']
-        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse(
             'blog:post_detail',
-            kwargs={'post_id': self.post_obj.id}
+            kwargs={'post_id': self.kwargs['comment_id']}
             )
 
 
@@ -229,18 +228,15 @@ class CommentDeleteView(LoginRequiredMixin, DeleteView):
     pk_url_kwarg = 'comment_id'
 
     def dispatch(self, request, *args, **kwargs):
-        self.post_obj = get_object_or_404(
-            Post,
-            id=kwargs['post_id'])
+        instance = get_object_or_404(
+            Comment,
+            id=kwargs['comment_id'])
+        if instance.author != request.user:
+            raise Http404
         return super().dispatch(request, *args, **kwargs)
-
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        form.instance.post_id = self.kwargs['post_id']
-        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse(
             'blog:post_detail',
-            kwargs={'post_id': self.post_obj.id}
+            kwargs={'post_id': self.kwargs['comment_id']}
             )
